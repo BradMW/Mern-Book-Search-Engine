c2503ca58af04f5860c45ba0e7e769d8a31a33e6
BradMW <bodell94@yahoo.com>
2021-12-04 00:17:37 -0600

    fixed mutations and login.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+// import { createUser } from '../utils/API';
+
 
 const SignupForm = () => {
   // set initial form state
